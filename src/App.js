10c8da3ffde66df5a5b1798bbd9be7174750f6b9
Gabriel Victor <133161909+GabrielVictorP@users.noreply.github.com>
2023-12-28 11:36:48 -0300

    Commit final
    Projeto concluído

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header/header";
+import RedesSociais from "./components/RedesSociais/redesSociais";
+
+const App = () => {
+  return (
+    <>
+    <Header/>
+      <RedesSociais/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Header/header.js
@@ -0,0 +1,14 @@
+import './header.css'
+
+const Header = () => {
+    return (
+      <div className="perfil_conteudo">
+        <div className="perfil_imagem"></div>
+          <h3>Gabriel Victor</h3>
+          <p>Desenvolvedor Front-end</p>
+        </div>
+    )
+  }
+  
+  export default Header;
+  
